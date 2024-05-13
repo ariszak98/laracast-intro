5ac1395ad0a82bc8cb08b2a7ba6450df0c4a0d8f
ariszak98 <ariszak98@icloud.com>
2024-05-13 18:33:19 +0300

    Fixed N+1 problem with Easy instead of Lazy Loading

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,14 +5,19 @@
     
     <h3>Hello from the Jobs page</h3><br><br>
 
-    <ul>
+    <div class="space-y-3">
         @foreach ($jobs as $job)
-            <li class="hover:underline hover:text-blue-800">
-                <a href="/jobs/{{ $job['id'] }}">
-                    <b>{{ $job['title'] }}:</b> Pays {{ $job['salary'] }} per year.
-                </a>
-            </li>
+            
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 hover:border-blue-400 rounded-lg">
+                
+                <div class="font-bold text-blue-400 text-sm">{{ $job->employer->name }}</div>
+                
+                <div>
+                <b>{{ $job['title'] }}:</b> Pays {{ $job['salary'] }} per year.
+                </div>
+            </a>
+            
         @endforeach
-    </ul>
+    </div>
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,8 @@ Route::get('/', function () {
 });
 
 Route::get('jobs', function () {
-    return view('jobs', ['jobs' => Job::all()]);
+    $jobs = Job::with('employer')->get();
+    return view('jobs', ['jobs' => $jobs]);
 });
 
 

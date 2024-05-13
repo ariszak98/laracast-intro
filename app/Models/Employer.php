4a21dc59e0584d889d154c10499b8dbc9d82e28d
ariszak98 <ariszak98@icloud.com>
2024-05-13 17:05:13 +0300

    Eloquent Relationships between Employer + Job

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Job;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Employer extends Model
 {
     use HasFactory;
+
+
+    /**
+     * Use a Employer Model to find ALL Job Models
+     * related to that model
+     * ( 1 Employer => MANY Jobs sooo => 'hasMany()' )
+     */
+    public function jobs(){
+        return $this->hasMany(Job::class);
+    }
+
+
 }
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -12,5 +13,13 @@ class Job extends Model {
     protected $table ='job_listings';
 
     protected $fillable = ['title', 'salary'];
+
+    /**
+     * Use a Job Model to find the SPECIFIC Employer Model
+     * connected to it. ( 1 Job => 1 Employer soooo => 'belongsTo()' )
+     */
+    public function employer() {
+        return $this->belongsTo(Employer::class);
+    }
     
 }

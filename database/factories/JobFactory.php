dd3eb192040139fee2edc6bce4b2f48c250bcd02
ariszak98 <ariszak98@icloud.com>
2024-05-13 16:35:10 +0300

    Added Factories for models:Job,Employer

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Employer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    use HasFactory;
+    
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            
+            'title' => fake()->jobTitle(),
+            'employer_id' => Employer::factory(),
+            'salary' => '$50,000'
+        ];
+    }
+}
